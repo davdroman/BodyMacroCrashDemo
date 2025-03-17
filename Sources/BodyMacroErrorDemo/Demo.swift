dabdd8a5f9ce0e69f857f0e697176f6a26739006
David Roman <2538074+davdroman@users.noreply.github.com>
2025-03-17 00:48:42 +0000

    WIP

--- Sources/BodyMacroErrorDemo/Demo.swift
+++ Sources/BodyMacroErrorDemo/Demo.swift
@@ -1,11 +1,13 @@
+import Dispatch
+
 @attached(body)
 public macro NoOpBodyMacro() = #externalMacro(module: "BodyMacro", type: "NoOpBodyMacro")
 
 class Demo {
-	@NoOpBodyMacro // removing this makes it compile! 🤯
+	@NoOpBodyMacro
 	func demo() {
-		Task { [weak self] in // removing [weak self] makes it compile! 🤯
-			// no-op, just the capture above is enough to trigger the build error
+		let doSomething = { [weak self] in
+			// no-op
 		}
 	}
 }
